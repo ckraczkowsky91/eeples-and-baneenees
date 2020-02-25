f75ae32168f3a211fa31f5e9c440d5256729af68
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-02-24 17:49:36 -0800

    Updated the index.js file.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from 'express';
 
 // initialize express
 const app = express();
-const PORT = 4000;
+const port = process.env.PORT || 4000;
 
 // define the endpoint for our main application
 app.get('/', (req, res) => {
   res.send(`Our application is running on port ${PORT}`)
 });
 
-app.listen(PORT, () =>
-  console.log(`Our application is running on port ${PORT}`)
+app.listen(port, () =>
+  console.log(`Our application is running on port ${port}`)
 );
