611d2f6f12002fac512cca58ec3ea2f45cf422b0
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-02-24 17:59:48 -0800

    Added an index.html file.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import path from 'path';
 
 // initialize express
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(express.static(path.join(__dirname)));
+
 // define the endpoint for our main application
 app.get('/', (req, res) => {
-  res.send(`Our application is running on port ${PORT}`)
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, () =>
