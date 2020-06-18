0d44ca668593e48ec05cf4ca3b6af46d57e1eaa7
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-06-18 16:36:09 -0700

    Added SwipeableDrawer by Material-UI to display details

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import GroceryList from './GroceryList';
-import GroceryForm from './GroceryForm';
 import GroceryListContainer from './GroceryListContainer';
-import GroceryListItems from './GroceryListItems';
 import {
   AppBar,
-  Box,
+  Divider,
+  Link,
+  List,
+  ListItem,
   Paper,
   IconButton,
+  SwipeableDrawer,
   Toolbar,
   Typography
 } from '@material-ui/core';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import { Menu } from '@material-ui/icons';
+
+const StyledLink = withStyles({
+  root: {
+    fontFamily: "Crete Round",
+    width: '100%'
+  }
+})(Link);
 
 const StyledPaper = withStyles({
   root: {
     margin: 'auto',
+    marginTop: '1vh',
+    marginBottom: '1.5vh',
     width: '75%',
   },
 })(Paper);
@@ -31,20 +41,64 @@ const StyledTitle = withStyles({
 const StyledAppBar = withStyles({
   root: {
     flexGrow: 1,
-    padding: "1.5ch",
+    padding: "0.5ch",
     background: '#ff6b6b'
   },
 })(AppBar);
 
+const StyledSwipeableDrawer = withStyles({
+  paper: {
+    padding: '10px',
+    width: '20%'
+  }
+})(SwipeableDrawer);
+
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      menuOpen: false
+    };
+  };
+
+  toggleDrawer(value){
+    this.setState({
+      menuOpen: value
+    })
+  };
+
   render(){
     return(
-      <div style={{backgroundColor: "#fdfcdc"}}>
+      <div style={{height: '100%'}}>
         <StyledAppBar position="sticky">
           <Toolbar>
+            <IconButton edge="start" color="inherit" onClick={() => this.toggleDrawer(true)}>
+              <Menu />
+            </IconButton>
             <StyledTitle variant="h4">Eeeples & Baneenees</StyledTitle>
           </Toolbar>
         </StyledAppBar>
+        <StyledSwipeableDrawer
+          open={this.state['menuOpen']}
+          onClose={() => {this.toggleDrawer(false)}}
+          onOpen={() => {this.toggleDrawer(true)}}>
+          <Typography style={{color: '#ff6b6b'}} variant="h4">About Me</Typography>
+          <Divider />
+          <Typography style={{color: '#4ecdc4', fontWeight: 'bold'}}>Developer</Typography>
+          <Typography style={{marginLeft: '8px'}}>Colin Kraczkowsky</Typography>
+          <Typography style={{color: '#4ecdc4', fontWeight: 'bold'}}>Resources</Typography>
+          <List>
+            <ListItem button>
+              <StyledLink href="https://github.com/ckraczkowsky91" target="_blank" rel="noreferrer">GitHub</StyledLink>
+            </ListItem>
+            <ListItem button>
+              <StyledLink href="https://medium.com/@ckraczkowsky" target="_blank" rel="noreferrer">Medium</StyledLink>
+            </ListItem>
+            <ListItem button>
+              <StyledLink href="https://www.linkedin.com/in/colinkraczkowsky/" target="_blank" rel="noreferrer">LinkedIn</StyledLink>
+            </ListItem>
+          </List>
+        </StyledSwipeableDrawer>
         <StyledPaper>
           <GroceryListContainer />
         </StyledPaper>
